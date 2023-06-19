9f33c416595eeef263bf8ac90743145118de3326
Muyiwv <47756707+Muyiwv@users.noreply.github.com>
2023-06-19 03:08:30 +0100

    Added files to repository

---
+++ Question 1a/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Question_1a;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student[] students = new Student[10]; // creating an array of students with 10 elements 
+        var info = "";
+        for (int i = 0; i < students.Length; i++) // looping through the lenght of the array to set the values for the student property
+        {
+            students[i] = new Student(); // creating a new student object 
+            students[i].Name = "STUDENT_" + i; // name +1 to make them unique
+            students[i].Id = "ID_" + i; // ID 
+            students[i].Age = 20 + i; // Age + the value of the element index
+        }
+
+        info = Student.GetInformation(students); // to get the value
+        Console.WriteLine("INFO: " + info); //. to print it out
+        _ = Console.ReadKey();
+    }
+}
---
+++ Question 1a/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Question_1a
+{
+    public class Student
+    {
+        public Student()
+        {
+        }
+        private string? name; // define the 3 properties of the student object
+        private string? id;
+        private int age;
+
+        public string Name
+        {
+            get { if (name == null) { return ""; } return name; } // defining a public getter and setter so the properties will be accessible outside of this class
+            set { name = value; }
+        }
+
+        public string Id
+        {
+            get { if (id == null) { return ""; } return id; }
+            set { id = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public static string GetInformation(Student[]? students) // declaring a method that takes an array of students
+        {
+            string studentInfo = "";
+            if (students == null) { return studentInfo; } // Checking that the value of our array is not null.
+            for (int i = 0; i < students.Length; i++)  // looping through the array of students to get the values in the object properties
+            {
+                studentInfo += " Name : " + students[i].Name + " Id : " + students[i].Id + " Age : " + students[i].Age + "\n";
+            }
+            return studentInfo; // returning the values from the student property
+        }
+    }
+}
+
